2f09153fb3f91cfda2483c7f61c4bbc4dd87fc11
Alexander Pustosmekhov <a.pustosmekhov@gmail.com>
2022-05-27 10:16:31 +0300

    fixes task 4 and 6

--- js/task-04.js
+++ js/task-04.js
@@ -2,16 +2,16 @@ let counterValue = 0;
 
 const btnDecrementRef = document.querySelector('[data-action="decrement"');
 const btnIncrementRef = document.querySelector('[data-action="increment"]');
-let counterRef = document.querySelector("#value").textContent;
+let counterRef = document.querySelector("#value");
 
 function decreaseValue() {
   counterValue += 1;
-  document.querySelector("#value").textContent = counterValue;
+  counterRef.textContent = counterValue;
 }
 
 function increaseValue() {
   counterValue -= 1;
-  document.querySelector("#value").textContent = counterValue;
+  counterRef.textContent = counterValue;
 }
 
 btnDecrementRef.addEventListener("click", () => increaseValue());
--- js/task-06.js
+++ js/task-06.js
@@ -5,10 +5,17 @@ inputRef.addEventListener("blur", (event) => {
     event.currentTarget.value.length ===
     Number(inputRef.getAttribute("data-length"))
   ) {
-    inputRef.classList.remove("invalid");
-    inputRef.classList.add("valid");
+    // inputRef.classList.remove("invalid");
+    // inputRef.classList.add("valid");
+    changeClass("valid", "invalid");
   } else {
-    inputRef.classList.remove("valid");
-    inputRef.classList.add("invalid");
+    // inputRef.classList.remove("valid");
+    // inputRef.classList.add("invalid");
+    changeClass("invalid", "valid");
   }
 });
+
+function changeClass(addClass, removeClass) {
+  inputRef.classList.remove(removeClass);
+  inputRef.classList.add(addClass);
+}
